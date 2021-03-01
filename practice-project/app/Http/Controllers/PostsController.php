1468fea4f1ba1becda8e5d4b94e7cd39ab797a88
dm1trycril <akrylov008@gmail.com>
2021-03-01 20:18:40 +0300

    Added a call to the recording function of the model, to write a post to a table, to the post controller

--- practice-project/app/Http/Controllers/PostsController.php
+++ practice-project/app/Http/Controllers/PostsController.php
@@ -21,17 +21,8 @@ class PostsController extends Controller
     }
     //You need to configure the controller to add data to the database
     public function add_post(PostAddingForm $req){
-    	//dd($req);
         $post = new Posts();
-
-        $post->author_id = $req->input('0');
-
-        $post->title = $req->input('title');
-        $post->body = $req->input('body');
-        $post->slug = $req->input('title');
-        
-        $post->save();
-
+        $post->add_post($req);
         return redirect()->route('news');
     }
 }
