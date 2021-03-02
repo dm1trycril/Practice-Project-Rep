ada2e308b74d3960938e5200526789bbb4abcc32
dm1trycril <akrylov008@gmail.com>
2021-03-02 09:52:36 +0300

    Made the display of posts using the slider

--- practice-project/resources/views/news.blade.php
+++ practice-project/resources/views/news.blade.php
@@ -4,12 +4,24 @@
 News
 @endsection
 
+@section('slick-cdn')
+    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+    <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+@endsection
+
 @section('content')
-	@foreach ($posts as $post)
-            <div class="alert alert-dark text-center col-4 offset-4">
+	<div class="slider">
+		@foreach ($posts as $post)
+            <div class="alert alert-dark text-center slider-item">
                 {!! $post->title !!}
                 <br/>
                 <a href="{{ route('show_post',  $post->id) }}" class="btn btn-dark btn" role="button" aria-pressed="true">Подробнее</a>
             </div>
-    @endforeach
+    	@endforeach
+	</div>
+	<script>
+		$(document).ready(function(){
+			$('.slider').slick();
+		})
+	</script>
 @endsection
